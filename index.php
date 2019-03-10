fbc1f40d4b441008d71e54923ab97c9bbebb5187
jonazapata <48414495+jonazapata@users.noreply.github.com>
2019-03-10 12:08:21 -0600

    index.php
    main project code

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+include("conexion.php");
+
+$conexion=conectarse();
+
+//main
+//echo "conexion exitosa";
+//en graficas se colocaran las estadisticas necesarias para hacer analisis de los procesos, tiempos, mejora continua etc
+//en historial se colocara el avance de los n/p en el tiempo
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	 <div>
+	   <header class="header">
+	 	     <img src="img/flujo.png">
+	 	     <h1>Material in Hold</h1>
+
+	 	     <!--<ul class="menuh">
+	 	       <li><a href="#">Home</a></li>
+	 		   <li><a href="#">Acerca de</a></li>
+	 		   <li><a href="#">Ayuda    </a></li>
+	 		   <li><a href="#">Contacto </a></li>
+	 	    </ul>-->
+
+	 	    <nav class="navegacion">
+	 	<ul class="menu">
+	 		<li><a href="//localhost/materialinhold/">DashBoard        </a></li>
+	 		<li><a href="#">Usuarios         </a>
+               <ul class="submenu">
+               	  <li><a href="//localhost/materialinhold/reg_user.php">Busqueda de Usuarios</a></li>
+               	  <!--<li><a href="#">Registro de Usuarios   </a></li>
+               	  <li><a href="#">Eliminacion de Usuarios</a></li>-->
+               </ul>
+	 		</li>
+	 		<li><a href="#">Tracking Material</a>
+               <ul class="submenu">
+               	  <li><a href="#">Registro   </a></li>
+               	  <li><a href="#">Seguimiento</a></li>
+               	  <li><a href="#">Issues     </a></li>
+               </ul>
+	 		</li>
+	 		<li><a href="#">Reportes         </a>
+               <ul class="submenu">
+               	  <li><a href="#">Historial</a></li>
+               	  <li><a href="#">Graficas </a></li>
+               	  <li><a href="#">Procesos</a></li>
+               </ul>
+	 		</li>
+	 	</ul>
+	 	</nav>
+	   </header>
+	 </div>
+</body>
+</html>
---
+++ reg_user.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	 <div>
+	   <header class="header">
+	 	     <img src="img/flujo.png">
+	 	     <h1>Material in Hold</h1>
+
+	 	     <!--<ul class="menuh">
+	 	       <li><a href="#">Home    </a></li>
+	 		   <li><a href="#">Acerca de</a></li>
+	 		   <li><a href="#">Ayuda    </a></li>
+	 		   <li><a href="#">Contacto </a></li>
+	 	    </ul>-->
+
+	 	    <nav class="navegacion">
+	 	<ul class="menu">
+	 		<li><a href="//localhost/materialinhold/">DashBoard        </a></li>
+	 		<li><a href="#">Usuarios         </a>
+               <ul class="submenu">
+               	  <li><a href="//localhost/materialinhold/reg_user.php">Busqueda de Usuarios</a></li>
+               	  <!--<li><a href="#">Registro de Usuarios   </a></li>
+               	  <li><a href="#">Eliminacion de Usuarios</a></li>-->
+               </ul>
+	 		</li>
+	 		<li><a href="#">Tracking Material</a>
+               <ul class="submenu">
+               	  <li><a href="#">Registro   </a></li>
+               	  <li><a href="#">Seguimiento</a></li>
+               	  <li><a href="#">Issues     </a></li>
+               </ul>
+	 		</li>
+	 		<li><a href="#">Reportes         </a>
+               <ul class="submenu">
+               	  <li><a href="#">Historial</a></li>
+               	  <li><a href="#">Graficas </a></li>
+               	  <li><a href="#">Procesos</a></li>
+               </ul>
+	 		</li>
+	 	</ul>
+	 	</nav>
+	   </header>
+	 </div>
+</body>
+</html>
